edc0f75973d58cb65459652eac4f067372a881a0
David Pelley <davidapelley@gmail.com>
2020-06-27 18:52:25 -0500

    adding keywords/expressions

---
+++ keywords_expressions.java
@@ -0,0 +1,17 @@
+public class keywords_expressions{
+	public static void main (String [] args){
+	    //keywords & expressions
+	           // a mile is equal to 1.609344 kilometers
+	           double kilometers = (100 * 1.609344);
+	           int highScore = 50;
+	           if (highScore == 50){
+	               System.out.println("This is an expression");
+	           }
+		
+		
+		
+		
+		
+	}
+	
+}
