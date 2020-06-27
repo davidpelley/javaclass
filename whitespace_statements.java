34381d7f451773d704648093121e3fbf1519dac7
David Pelley <davidapelley@gmail.com>
2020-06-27 18:59:31 -0500

    adding whitespace/statements

---
+++ whitespace_statements.java
@@ -0,0 +1,21 @@
+public class whitespace_statements{
+	public static void main(String [] args){
+        //whitespace, statements and indentation
+        int                  var
+                =
+                50
+                ;
+        var++;
+        var--;
+        System.out.println("test");
+        System.out.println("this"+
+                " is another"+
+                " still more");
+        int other = 50;var--; System.out.println("this is another one ");
+		
+		
+		
+		
+		
+	}
+}
