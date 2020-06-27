987305400187429dd4336a31f02f224f27cecde3
David Pelley <davidapelley@gmail.com>
2020-06-27 17:56:38 -0500

    Adding if_then

---
+++ if_then.java
@@ -0,0 +1,17 @@
+public class if_then{
+	public static void main(String [] args){
+        //if-then
+        boolean isAlien = false;
+        if (isAlien == true) {
+            System.out.println("It is not an alien");
+            System.out.println("And I need the braces here");
+        }
+        if(isAlien == true);   // java sucks
+             System.out.println("It is not an alien");
+		
+		
+		
+	}
+	
+	
+}
