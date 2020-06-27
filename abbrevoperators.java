17ffd0dd80b625663bcc1d590e0535e14e48dbc0
David Pelley <davidapelley@gmail.com>
2020-06-27 17:46:22 -0500

    Adding abbrevoperators source/class

---
+++ abbrevoperators.java
@@ -0,0 +1,22 @@
+public class abbrevoperators{
+	
+	public static void main (String [] args){
+		
+		int result = 1; 
+		// result = result + 1
+		result++; 
+		System.out.println(result);
+		result--; 
+		System.out.println(result);
+		result += 2; 
+		System.out.println(result);
+		result *= 2; 
+		System.out.println(result);
+		result /= 2; 
+		System.out.println(result);
+		result -= 2; 
+		System.out.println(result);
+		
+	}
+	
+}
