6649a060fb0d7b0b1afbef9c6d8a001891daacf0
David Pelley <davidapelley@gmail.com>
2020-06-27 20:46:38 -0500

    fixed speedconverter and wrote seperate main

---
+++ main.java
@@ -0,0 +1,12 @@
+public class main{
+	
+	public static void main (String[] args){
+		double miles = speedconverter.toMilesPerHour(10.5);
+		System.out.println("Miles = " + miles);
+		speedconverter.printConversion(10.5);
+		
+		
+	}
+	
+	
+}
--- speedconverter.java
+++ speedconverter.java
@@ -1,14 +1,20 @@
 import java.util.*;
 public class speedconverter{
-	public static void main (String [] args){
-		toMilesPerHour(100);
-		
-	}
 	public static long toMilesPerHour(double kilometersPerHour){
 		if (kilometersPerHour < 0){
 			return -1;
 		}
-		return Math.round((double) kilometersPerHour / 1.609);
+		return Math.round(kilometersPerHour / 1.609);
 		 
+	}
+	public static void printConversion(double kilometersPerHour){
+		if (kilometersPerHour < 0){
+			System.out.println("Invalid value");
+		}
+		else {
+			long milesPerHour = toMilesPerHour(kilometersPerHour);
+			System.out.println(kilometersPerHour + " km/h " + milesPerHour + " mi/h");
+			
+		}
 	}
 }
